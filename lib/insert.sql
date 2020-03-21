7b96cd30c684e03e5c26380d5cbb008b4979d08a
James Rogers <jarogers095@gmail.com>
2020-03-21 00:18:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (
 ("Sea of Stars", "Games", 92346, "03/18/2020", "04/18/2020"),
-("MIND MGMT","Games", 92346, "03/18/2020", "04/18/2020"),
-("Chronicles of Crime","Games", 92346, "03/18/2020", "04/18/2020"),
-("Swords n Magic and Stuff","Games", 92346, "03/18/2020", "04/18/2020"),
-("Multiverse","Games", 92346, "03/18/2020", "04/18/2020"),
-("Bold Made","Games", 92346, "03/18/2020", "04/18/2020"),
-("Ruination","Games", 92346, "03/18/2020", "04/18/2020"),
-("Pax Pamir","Games", 92346, "03/18/2020", "04/18/2020"),
-("Tiny Cthulhu","Games", 92346, "03/18/2020", "04/18/2020"),
-("","Games", 92346, "03/18/2020", "04/18/2020"))
+("MIND MGMT","Games", 42168, "03/18/2020", "04/18/2020"),
+("Chronicles of Crime","Games", 78123, "03/18/2020", "04/18/2020"),
+("Swords n Magic and Stuff","Games", 64851, "03/18/2020", "04/18/2020"),
+("Multiverse","Games", 64328, "03/18/2020", "04/18/2020"),
+("Bold Made","Games", 79461, "03/18/2020", "04/18/2020"),
+("Ruination","Games", 97643, "03/18/2020", "04/18/2020"),
+("Pax Pamir","Games", 19735, "03/18/2020", "04/18/2020"),
+("Tiny Cthulhu","Games", 58214, "03/18/2020", "04/18/2020"),
+("Nekojishi","Games", 69874, "03/18/2020", "04/18/2020"))
