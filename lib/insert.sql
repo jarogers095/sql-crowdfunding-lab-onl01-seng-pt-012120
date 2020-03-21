c47548d12b5cdc6e15f87e9e4781532b8c45651b
James Rogers <jarogers095@gmail.com>
2020-03-21 00:30:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,16 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+("Sea of Stars", "Games", 92346, "03/18/2020", "04/18/2020"),
+("MIND MGMT","Games", 42168, "03/18/2020", "04/18/2020"),
+("Chronicles of Crime","Games", 78123, "03/18/2020", "04/18/2020"),
+("Swords n Magic and Stuff","Games", 64851, "03/18/2020", "04/18/2020"),
+("Multiverse","Games", 64328, "03/18/2020", "04/18/2020"),
+("Bold Made","Games", 79461, "03/18/2020", "04/18/2020"),
+("Ruination","Games", 97643, "03/18/2020", "04/18/2020"),
+("Pax Pamir","Games", 19735, "03/18/2020", "04/18/2020"),
+("Tiny Cthulhu","Games", 58214, "03/18/2020", "04/18/2020"),
+("Nekojishi","Games", 69874, "03/18/2020", "04/18/2020");
 
-
-INSERT INTO users VALUES (
+INSERT INTO users VALUES 
 ("Bob", 50),
 ("Joe", 30),
 ("Steve", 35),
@@ -20,4 +30,4 @@ INSERT INTO users VALUES (
 ("Wilhelmina", 16),
 ("Rheinhardt", 66),
 ("Kristen", 42),
-("Alyson", 46));
+("Alyson", 46);
