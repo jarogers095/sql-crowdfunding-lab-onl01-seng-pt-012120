e42355265760804102cd64379ad18fe3ca1991ef
James Rogers <jarogers095@gmail.com>
2020-03-21 00:19:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,4 +8,6 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 ("Ruination","Games", 97643, "03/18/2020", "04/18/2020"),
 ("Pax Pamir","Games", 19735, "03/18/2020", "04/18/2020"),
 ("Tiny Cthulhu","Games", 58214, "03/18/2020", "04/18/2020"),
-("Nekojishi","Games", 69874, "03/18/2020", "04/18/2020"))
+("Nekojishi","Games", 69874, "03/18/2020", "04/18/2020"));
+
+INSERT INTO users ()
