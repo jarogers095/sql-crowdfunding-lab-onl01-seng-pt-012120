70a20b8b8b8f7411abf376e5de2567f5579cab09
James Rogers <jarogers095@gmail.com>
2020-03-21 00:14:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (
 ("Sea of Stars", "Games", 92346, "03/18/2020", "04/18/2020"),
+("","Games",92346, "03/18/2020", "04/18/2020"),
