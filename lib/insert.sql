1ca7409674fb27ece2e0796cadc256b4bd389f01
James Rogers <jarogers095@gmail.com>
2020-03-21 00:16:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,11 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (
 ("Sea of Stars", "Games", 92346, "03/18/2020", "04/18/2020"),
-("","Games",92346, "03/18/2020", "04/18/2020"),
+("MIND MGMT","Games", 92346, "03/18/2020", "04/18/2020"),
+("Chronicles of Crime","Games", 92346, "03/18/2020", "04/18/2020"),
+("Swords n Magic and Stuff","Games", 92346, "03/18/2020", "04/18/2020"),
+("Multiverse","Games", 92346, "03/18/2020", "04/18/2020"),
+("Bold Made","Games", 92346, "03/18/2020", "04/18/2020"),
+("Ruination","Games", 92346, "03/18/2020", "04/18/2020"),
+("Pax Pamir","Games", 92346, "03/18/2020", "04/18/2020"),
+("","Games", 92346, "03/18/2020", "04/18/2020"),
+("","Games", 92346, "03/18/2020", "04/18/2020"))
